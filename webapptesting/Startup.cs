847544cf1381f3463559095306a5e0036afb2b5c
muhammad.faizan <muhammad.faizan@NX-PC03>
2019-03-29 17:42:41 +0500

    webapptesting 2nd commit

---
+++ webapptesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webapptesting.Startup))]
+namespace webapptesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
